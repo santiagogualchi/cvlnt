3fad859c2b2c50a13694d79d6f5c76d5f28012d5
Santiago Gualchi <santiagogualchi@gmail.com>
2021-01-09 18:47:06 -0300

    Implement get_transactions

---
+++ R/get_transactions.R
@@ -0,0 +1,32 @@
+#' Get transactions
+#'
+#' @param address The wallet address.
+#' @param api_key The user's public API key. To generate it, you need an account
+#' at [Covalent HQ](https://www.covalenthq.com/).
+#' @param chain_id Chain ID of the Blockchain being queried. Supports "137" for
+#' Matic, defaults to "1" for Ethereum Mainnet.
+#' @param sleep Interval in seconds between requests.
+#' @param ... Additional arguments to pass to the request.
+#'
+#' @return
+#' Reurns all transactions for `address` including their decoded log events as a
+#' list on the `log_events` column.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' address <- "0x5a6d3b6bf795a3160dc7c139dee9f60ce0f00cae"
+#'
+#' get_transactions(address, MY_API_KEY)
+#' get_transactions(address, MY_API_KEY, "quote-currency" = "eth")
+#' }
+get_transactions <- function(address, api_key, chain_id = "1", sleep = 1/10,
+                             ...) {
+  endpoint <- paste0("https://api.covalenthq.com/v1/",
+                     chain_id,
+                     "/address/",
+                     address,
+                     "/transactions_v2/")
+
+  get_paginated_endpoint(endpoint, api_key, sleep = sleep, ...)
+}
