43026deb0b747d1756b0ee2ee0055e3c5c0b3943
Santiago Gualchi <santiagogualchi@gmail.com>
2021-01-10 14:17:22 -0300

    Fix parameter order

--- R/get_address_balances.R
+++ R/get_address_balances.R
@@ -3,9 +3,9 @@
 #' @param address The wallet address.
 #' @param api_key The user's public API key. To generate it, you need an account
 #' at [Covalent HQ](https://www.covalenthq.com/).
-#' @param ... Additional arguments to pass to the request.
 #' @param chain_id Chain ID of the Blockchain being queried. Supports "137" for
 #' Matic, defaults to "1" for Ethereum Mainnet.
+#' @param ... Additional arguments to pass to the request.
 #'
 #' @return
 #' Returns a data.frame with all ERC20 and NFT token balances along with their
@@ -19,7 +19,7 @@
 #' get_address_balances(address, MY_API_KEY)
 #' get_address_balances(address, MY_API_KEY, "quote-currency" = "eth")
 #' }
-get_address_balances <- function(address, api_key, ..., chain_id = "1") {
+get_address_balances <- function(address, api_key, chain_id = "1", ...) {
   endpoint <- paste0("https://api.covalenthq.com/v1/",
                      chain_id,
                      "/address/",
