4931e622df87b314a6bde83715da9d149e154fb9
Santiago Gualchi <santiagogualchi@gmail.com>
2021-01-10 16:05:18 -0300

    Fix missing parameter

--- R/get_block.R
+++ R/get_block.R
@@ -18,10 +18,10 @@
 #' get_block("11601973", MY_API_KEY)
 #' get_block("latest", MY_API_KEY)
 #' }
-get_block <- function(block_height, api_key, ...) {
+get_block <- function(block_height, api_key, sleep = 1/10, ...) {
   endpoint <- paste0("https://api.covalenthq.com/v1/1/block_v2/",
                      block_height,
                      "/")
 
-  get_paginated_endpoint(endpoint, api_key, sleep = sleep, ...)
+  get_paginated_endpoint(endpoint, api_key, sleep, ...)
 }
